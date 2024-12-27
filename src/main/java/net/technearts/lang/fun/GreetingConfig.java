d0aa6344555481e337a16eddb0567e64d366b26c
Paulo Suderio <paulo.suderio@gmail.com>
2024-12-27 10:22:23 -0300

    Initial commit

---
+++ src/main/java/net/technearts/lang/fun/GreetingConfig.java
@@ -0,0 +1,12 @@
+package net.technearts.lang.fun;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
